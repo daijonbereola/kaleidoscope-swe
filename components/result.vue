5f2d473a3a16432107b976e5dcf935cff0e6b93b
daijonbereola <54553857+daijonbereola@users.noreply.github.com>
2019-12-04 12:07:59 -0500

    result.vue

---
+++ components/result.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card>
+        <v-card-text>
+            <v-card
+                    max-height="300px"
+                    flat
+                    tile
+                    v-for="p in p_info.swatches"
+                    v-bind:key="p.key"
+                    :color="p.color"
+                    :height="p.height"
+            >
+                {{ p }}
+            </v-card>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "result",
+        props: {
+            palette_info: Object
+        },
+        data () {
+            return {
+                p_info: this.palette_info
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
